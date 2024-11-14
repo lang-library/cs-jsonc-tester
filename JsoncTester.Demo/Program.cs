91e171e256d54d3ca0adceb4fc1dbdd56f034681
JavaCommons Technologies <javacommons@gmail.com>
2024-11-14 21:16:55 +0900

    .

---
+++ JsoncTester.Demo/Program.cs
@@ -0,0 +1,22 @@
+﻿using Global;
+using System;
+using Xunit;
+namespace Main;
+
+static class Program
+{
+    [STAThread]
+    static void Main(string[] originalArgs)
+    {
+        var t1 = JsoncTester.FromObject(
+            new { x = 123, y = 456 });
+        var t2 = JsoncTester.FromObject(
+            new { y = 456, x = 123 });
+        var t3 = JsoncTester.FromObject(
+            new { x = 123, y = 4567 });
+        Console.WriteLine(JsoncTester.DeepEquals(t1, t2));
+        Console.WriteLine(JsoncTester.DeepEquals(t1, t3));
+        Console.WriteLine(JsoncTester.JsonEquals(JsoncTester.ToJson(t1), JsoncTester.ToJson(t2)));
+        Console.WriteLine(JsoncTester.JsonEquals(JsoncTester.ToJson(t1), JsoncTester.ToJson(t3)));
+    }
+}
---
+++ JsoncTester/JsoncTester.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+
+namespace Global;
+
+public class JsoncTester
+{
+    public static bool JsonEquals(string json1, string json2)
+    {
+        var o1 = FromJson(json1);
+        var o2 = FromJson(json2);
+        return JObject.DeepEquals(o1, o2);
+    }
+    //
+    public static bool DeepEquals(object x, object y)
+    {
+        var o1 = FromObject(x);
+        var o2 = FromObject(y);
+        return JObject.DeepEquals(o1, o2);
+    }
+    public static string ToJson(dynamic x, bool indent = false)
+    {
+        return JsonConvert.SerializeObject(x, indent ? Formatting.Indented : Formatting.None);
+    }
+    public static dynamic? FromJson(string json)
+    {
+        if (String.IsNullOrEmpty(json)) return null;
+        return JsonConvert.DeserializeObject(json, new JsonSerializerSettings
+        {
+            DateParseHandling = DateParseHandling.None
+        });
+    }
+    public static dynamic? FromObject(dynamic x)
+    {
+        if (x == null) return null;
+        var o = (dynamic)JObject.FromObject(new { x = x },
+            new JsonSerializer
+            {
+                DateParseHandling = DateParseHandling.None
+            });
+        return o.x;
+    }
+}
